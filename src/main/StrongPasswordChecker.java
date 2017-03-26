6c25a1416f6eb119a1a6cc9b3a90b8bb754bcb27
Fernanda Bernardo <fernandabernardo94@gmail.com>
2017-03-26 11:42:11 -0300

    password has between 6 and 20 characters

--- src/main/StrongPasswordChecker.java
+++ src/main/StrongPasswordChecker.java
@@ -2,4 +2,9 @@ package main;
 
 public class StrongPasswordChecker {
 
+	public static int check(String password) {
+		if(password.length() >= 6 && password.length() <= 20) return 0;
+		return 1;
+	}
+
 }
--- src/test/StrongPasswordCheckerTest.java
+++ src/test/StrongPasswordCheckerTest.java
@@ -1,14 +1,19 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+
+import main.StrongPasswordChecker;
 
 public class StrongPasswordCheckerTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void shouldHasBetween6And20Characters() {
+		assertEquals(0, StrongPasswordChecker.check("password")); // >6 && <20 characters
+		assertEquals(0, StrongPasswordChecker.check("passwo")); // 6 characters
+		assertEquals(0, StrongPasswordChecker.check("passwordpasswordpass")); // 20 characters
+		assertEquals(1, StrongPasswordChecker.check("passw")); // <6 characters
+		assertEquals(1, StrongPasswordChecker.check("passwordpasswordpassw")); // >20 characters
 	}
-
 }
