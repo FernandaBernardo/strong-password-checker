17b88319bbce8f6c8f4b5f4f638299aeba70bd13
Fernanda Bernardo <fernandabernardo94@gmail.com>
2017-07-02 20:13:05 -0300

    change code to chain of responsibility

--- src/main/PasswordType.java
+++ src/main/PasswordType.java
@@ -1,18 +1,64 @@
 package main;
 
 public enum PasswordType {
-	STRONG (0, "The password is strong"),
-	INVALID (1, "The password is invalid"),
-	SMALL_PASS (1, "The password must have at least 6 characters"),
-	BIG_PASS (1, "The password must have less than 20 characters"),
-	UPPERCASE (1, "The password must have at least one uppercase character"),
-	LOWERCASE (1, "The password must have at least one lowercase character"),
-	DIGIT (1, "The password must have at least one digit"),
-	REPEATED_CHARACTERS(1, "The password must NOT contain three repeating characters")
+	STRONG (0, "The password is strong") {
+		@Override
+		public String condition(String password) {
+			return getMessage();
+		}
+	},
+	SMALL_PASS (1, "The password must have at least 6 characters") {
+		@Override
+		public String condition(String password) {
+			if(password.length() < 6) return getMessage();
+			else return BIG_PASS.condition(password);
+		}
+	},
+	BIG_PASS (1, "The password must have less than 20 characters") {
+		@Override
+		public String condition(String password) {
+			if(password.length() > 20) return getMessage();
+			else return UPPERCASE.condition(password);
+		}
+	},
+	UPPERCASE (1, "The password must have at least one uppercase character") {
+		@Override
+		public String condition(String password) {
+			if(password.equals(password.toLowerCase())) return getMessage();
+			else return LOWERCASE.condition(password);
+		}
+	},
+	LOWERCASE (1, "The password must have at least one lowercase character") {
+		@Override
+		public String condition(String password) {
+			if(password.equals(password.toUpperCase())) return getMessage();
+			else return PasswordType.DIGIT.condition(password);
+		}
+	},
+	DIGIT (1, "The password must have at least one digit") {
+		@Override
+		public String condition(String password) {
+			if(!password.matches(".*\\d.*")) return getMessage();
+			else return PasswordType.REPEATED_CHARACTERS.condition(password);
+		}
+	},
+	REPEATED_CHARACTERS(1, "The password must NOT contain three repeating characters") {
+		@Override
+		public String condition(String password) {
+			for(int i = 0; i < password.length() - 2; i++) {
+				char firstChar = password.charAt(i);
+				if(firstChar == password.charAt(i+1) && firstChar == password.charAt(i+2)) {
+					return getMessage();
+				}
+			}
+			return STRONG.condition(password);
+		}
+	}
 	;
 
 	private int state;
 	private String message;
+	public String password;
 
 	private PasswordType(int state, String message) {
 		this.state = state;
@@ -26,5 +72,10 @@ public enum PasswordType {
 	public String getMessage() {
 		return message;
 	}
-
+	
+	public abstract String condition(String password);
+	
+	public static String execute(String password) {
+		return SMALL_PASS.condition(password);
+	}
 }
--- src/main/StrongPasswordChecker.java
+++ src/main/StrongPasswordChecker.java
@@ -3,37 +3,6 @@ package main;
 public class StrongPasswordChecker {
 
 	public static String check(String password) {
-		return checkType(password).getMessage();
+		return PasswordType.execute(password);
 	}
-
-	private static PasswordType checkType(String password) {
-		if(password.length() < 6) {
-			return PasswordType.SMALL_PASS;
-		}
-		if(password.length() > 20) {
-			return PasswordType.BIG_PASS;
-		}
-		boolean hasUppercaseChar = !password.equals(password.toLowerCase());
-		if(!hasUppercaseChar) {
-			return PasswordType.UPPERCASE;
-		}
-		boolean hasLowercaseChar = !password.equals(password.toUpperCase());
-		if(!hasLowercaseChar) {
-			return PasswordType.LOWERCASE;
-		}
-		boolean hasDigit = password.matches(".*\\d.*");
-		if(!hasDigit) {
-			return PasswordType.DIGIT;
-		}
-
-		for(int i = 0; i < password.length() - 2; i++) {
-			char firstChar = password.charAt(i);
-			if(firstChar == password.charAt(i+1) && firstChar == password.charAt(i+2)) {
-				return PasswordType.REPEATED_CHARACTERS;
-			}
-		}
-
-		return PasswordType.STRONG;
-	}
-
 }
